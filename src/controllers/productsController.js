5344a30a68ffa0c1b6132903c2e3820ae64035da
Kevin Vazquez <kv937398@gmail.com>
2024-10-18 15:27:14 -0300

    'Create' controller

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,6 +1,7 @@
 import { prisma } from "../../config/db.js"
 import path, { dirname } from "path"
 import { fileURLToPath } from "url"
+import fs from 'fs'
 
 const getAll = async (req, res) => {
     try {
@@ -27,12 +28,14 @@ const getById = async (req, res) => {
 
 const create = async (req, res) => {
     try {
-        if(!req.file) {
-            return res.status(500).json({message: 'There was no file uploaded'})
+        let imagename = '';
+        let imgPath = '';
+        
+        if(req.file) {
+            imagename = req.file.filename;
+            imgPath = path.join(dirname(fileURLToPath(import.meta.url)), 'uploads', imagename);
         }
-        const { title, description, price, stock} = req.body
-        const imagename = req.file.filename; // Save only the filename
-        const imgPath = path.join(dirname(fileURLToPath(import.meta.url)), 'uploads', imagename)
+        const { title, description, price, stock} = req.body 
     
         const imageMetadata = {
             img_url: imagename, // Store the filename only
@@ -51,4 +54,56 @@ const create = async (req, res) => {
 
 }
 
-export { getAll, getById, create }
+const updateController = async (req, res) => {
+    try {
+        const { title, description, price, stock, id } = req.body
+        const data = {title, description, price: Number(price), stock: Number(stock)}
+        if(req.file) {
+            data['img_url'] = req.file.filename
+        }
+        await prisma.products.update({
+            where: {
+                id: Number(id)
+            },
+            data: data
+        })
+
+        res.json({message: 'Funca'})
+    } catch (error) {
+        res.status(500).json({message: 'There was an error'})
+    }
+}
+
+const deleteController = async (req, res) => {
+    try {
+        const { id } = req.body
+        console.log('empieza');
+        
+        const result = await prisma.products.findUnique({
+            where: {
+                id: Number(id)
+            }
+        });
+
+        const imgPath = path.join(dirname(fileURLToPath(import.meta.url)), '..', 'uploads', result.img_url);
+
+        if(fs.existsSync(imgPath)) {
+            fs.unlink(imgPath, (err) => {
+                if(err) {
+                    console.error(err)
+                } else {
+                    console.log('Success')
+                }
+            })
+        }
+
+        await prisma.products.delete({where: {id: Number(id)}})
+
+        res.json({message: 'Deleted successfully'});
+    } catch (error) {
+        /* res.status(500).json({message: 'There was an error'}) */
+        res.sendStatus(500);
+    }
+}
+
+export { getAll, getById, create, updateController, deleteController }
