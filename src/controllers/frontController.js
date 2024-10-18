e60b60b620d2969c07a596f1ad538f359f82a4fc
Kevin Vazquez <kv937398@gmail.com>
2024-10-18 15:24:40 -0300

    added the update part

--- src/controllers/frontController.js
+++ src/controllers/frontController.js
@@ -1,14 +1,27 @@
-/* import {dirname} from 'path'
-import fs from 'fs'
-import path from 'path'
-import { fileURLToPath } from 'url' */
-import { prisma } from '../../config/db.js'
+import { prisma } from "../../config/db.js";
 
-const showAll = (req, res) => {
-    /* const data = JSON.parse(fs.readFileSync(path.join(dirname(fileURLToPath(import.meta.url)), '..', 'data', 'images.json')))
-    res.render('show', {data}) */
-    const data = prisma.products.findMany()
-    res.json({data})
+const showAll = async (req, res) => {
+  try {
+    const data = await prisma.products.findMany()
+    res.render("show", { data })
+  } catch (error) {
+    res.send('error')
+  }
 }
 
-export {showAll}
+const update = async (req, res)=> {
+    try {
+        const { id } = req.query
+        const data = await prisma.products.findUnique({
+            where: {
+                id: parseInt(id)
+            }
+        })
+        res.render('update', {data})
+    } catch (error) {
+        console.error(error)
+        res.sendStatus(500)
+    }
+}
+
+export { showAll, update };
