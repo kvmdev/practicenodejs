64d76f22f87ce0a85193d30d13c35bcdf9bae89a
Kevin Vazquez <kv937398@gmail.com>
2024-10-30 16:41:05 -0300

    Added the functionality to make it show a limited amount of products

--- src/controllers/frontController.js
+++ src/controllers/frontController.js
@@ -2,8 +2,19 @@ import { prisma } from "../../config/db.js";
 
 const showAll = async (req, res) => {
   try {
-    const data = await prisma.products.findMany()
-    res.render("show", { data })
+    const limit = 10
+    const offset = parseInt(req.query.offset) || 1
+    let skip = (offset * limit) - limit;
+
+    const data = await prisma.products.findMany({
+      take: limit,
+      skip
+    })
+
+    let count = await prisma.products.count();
+    count = count > 10 ? parseInt(count / 10) + 1 : 1;
+
+    res.render("show", { data, count })
   } catch (error) {
     res.send('error')
   }
