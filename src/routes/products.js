814d3448af1536af955964b3f1feadcc8910ff6e
Kevin Vazquez <kv937398@gmail.com>
2024-11-05 15:40:49 -0300

    Converted the validate middleware to a dynamic validator depending of the schema

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,16 +1,18 @@
 import { Router } from "express"
-import { createController, deleteController, getAllController, updateController } from "../controllers/productsController.js"
+import { createController, deleteController, getAllController, getById, updateController } from "../controllers/productsController.js"
 import { storage } from "../service/multerUploader.js"
 import multer from "multer"
 import { validate } from "../middlewares/validator.js"
 const upload = multer({storage})
+import productSchema from "../schemas/Product.js"
 
 
 const router = Router()
 
 router.get('/', getAllController)
-router.post('/', upload.single('image'), validate, createController)
-router.put('/', upload.single('image'), validate, updateController)
+router.get('/:id', getById)
+router.post('/', upload.single('image'), (req, res, next) =>  validate(req, res, next, productSchema), createController)
+router.put('/', upload.single('image'), (req, res, next) =>  validate(req, res, next, productSchema), updateController)
 router.delete('/', deleteController)
 
 export default router
