a7df991268dfd54831813856a3a2f38b416cd944
Kevin Vazquez <kv937398@gmail.com>
2024-11-05 15:41:17 -0300

    Added category_id to the schema

--- src/schemas/Product.js
+++ src/schemas/Product.js
@@ -2,9 +2,10 @@ import {z} from 'zod'
 
 const productSchema = z.object({
     title: z.string().min(1).max(100),
-    description: z.string().min(1).max(150),
-    price: z.string().min(1),
-    stock: z.string().min(1)
+    description: z.string().min(1).max(200),
+    price: z.number(),
+    stock: z.number(),
+    category_id: z.number()
 })
 
 export default productSchema
