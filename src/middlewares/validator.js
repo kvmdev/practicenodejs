a1c2d98964b1c1f19c2506abf5cbd08bd6f59b4c
Kevin Vazquez <kv937398@gmail.com>
2024-11-05 15:39:12 -0300

    Converted some parameters from body to number

--- src/middlewares/validator.js
+++ src/middlewares/validator.js
@@ -1,14 +1,44 @@
-import productSchema from "../schemas/Product.js";
+import { prisma } from "../../config/db.js";
 
-const validate = (req, res, next) => {
-    const parseResult = productSchema.safeParse(req.body)
-    if(!parseResult.success) {
-        return res.status(400).json({errors: parseResult.error.errors})
+const validate = (req, res, next, schema) => {
+  try {
+    req.body.price = Number(req.body.price);
+    req.body.stock = Number(req.body.stock);
+    req.body.category_id = Number(req.body.category_id);
+    
+    const parseResult = schema.safeParse(req.body);
+    
+    // If validation fails, send an error response and return early
+    if (!parseResult.success) {
+      return res.status(400).json({ errors: parseResult.error.errors });
     }
+    
+    const category_id = req.body.category_id;
 
-    req.validatedData = parseResult.data
+    // Find the category in the database
+    prisma.category.findUnique({
+        where: {
+          id: category_id
+        },
+      })
+      .then((val) => {
+        if (!val) {
+          // If category not found, send a 404 error
+          return res.status(404).json({ message: "Category not found" });
+        }
+          req.validatedData = req.body;
+          next()
+      })
+      .catch((err) => {
+        // If there's an error with the database query, send a 500 error
+        console.error(err);
+        return res.status(500).json({ message: "There was an error", err });
+      });
+    
+  } catch (error) {
+    // Catch any other errors and send a 400 response
+    return res.status(400).json({ message: "Not enough arguments" });
+  }
+};
 
-    next()
-}
-
-export {validate}
+export { validate };
